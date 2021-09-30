39479c32543dccef6b0b3f9dc8e53027e88f720e
Tyler Mozo <Tmozo001@gmail.com>
2021-09-30 16:55:42 -0400

    first commit

---
+++ src/main/java/exercise24/solution24.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Tyler Mozo
+ */
+
+package exercise24;
+import java.util.Scanner;
+
+
+public class solution24 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static String getString1(){
+        /*
+        ask user for a string
+        assign it to a string varaible
+        return string1
+         */
+    }
+    private static String getString2()
+        /*
+        ask user for a string
+        assign it to a string varaible
+        return string2
+         */
+
+    }
+    public static int length(String s, char ch) {
+        /*
+        set up a loop to go through each string
+        check the length of s1
+        then 2
+        return n, counter to determine how many loops went through giving the length
+         */
+    }
+
+
+
+
+    static void main(String[] args) {
+
+    }
+
+
+
+
+}
