da56cec1a5b631b6bbc0e7277eb1935d15684030
Tyler Mozo <Tmozo001@gmail.com>
2021-09-30 22:30:53 -0400

    challenging exercise25 COMPLETE!

--- exercise25/src/main/java/exercise25/solution25.java
+++ exercise25/src/main/java/exercise25/solution25.java
@@ -12,27 +12,79 @@ public class solution25 {
 
     private static String getPass(){
         //ask user for pass
+        System.out.print("Enter your password: ");
         //scan in pass
+        String pass = input.next();
         //return pass
+        return pass;
     }
     private static void passwordValidator(String pass){
         // use a variable to access java built in function length
-        //check if the pass has lower case and uppercase
+        int n = pass.length();
+
+        //assign each case to false, in order to flag true when true
+        boolean hasLower = false;
+        boolean hasUpper = false;
+        boolean hasDigit = false;
+        boolean specialChar = false;
+
+        //giving what falls into the specialCharacter category
+        Set<Character> specialCharacter = new HashSet<Character>(Arrays.asList('!', '@', '#',
+                '$', '%', '^', '&', '*', '(', ')', '.', '-', '+', ';', ':'));
+//going character by character running each of these tests
+        for (char i : pass.toCharArray()) {
+            //check if the character is a lower case
+            if (Character.isLowerCase(i))
+                hasLower = true;
+            //check if the character is a upper case
+            if (Character.isUpperCase(i))
+                hasUpper = true;
+            //check if the character is a digit
+            if (Character.isDigit(i))
+                hasDigit = true;
+            //check if the character is a special character
+            if (specialCharacter.contains(i))
+                specialChar = true;
+        }
+//if all criteria is met (special, digit, upper, lower)
+        if (hasDigit && hasLower || hasUpper && specialChar
+                && (n >= 8))
+            System.out.printf("The password \'%s\' is a very strong", pass);
+//if either upper or lower or special
+        else if ((hasLower || hasUpper || specialChar)
+                && (n >= 8))
+            System.out.printf("The password \'%s\' is a strong", pass);
+//if only has upper and lower
+        else if(hasLower || hasUpper && (n < 8))
+            System.out.printf("The password \'%s\' is a weak", pass);
+//else if only digits
+        else
+            System.out.printf("The password \'%s\' is a very weak", pass);
+
         // check if pass has special characters
+        //check if its a number
         //loop through each character of the pass and check what it is
         //assign each of these values to a number
         //if number is above a certain threshold (>=8 , ...)
         //sum together to get a total or if higher print if 1 && 2 print....
+
     }
-
-
-
-
-
 
     public static void main(String[] args) {
         //call getPass
         String pass = getPass();
         //call passwordValidation to do all the heavy lifting
+        passwordValidator(pass);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
