82319b50fc002841194de0643bc97def376d5cc3
Tyler Mozo <Tmozo001@gmail.com>
2021-09-30 21:49:58 -0400

    Skeleton created for exercise 25

---
+++ exercise25/src/main/java/exercise25/solution25.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Tyler Mozo
+ */
+package exercise25;
+import java.io.*;
+import java.util.*;
+
+
+public class solution25 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static String getPass(){
+        //ask user for pass
+        //scan in pass
+        //return pass
+    }
+    private static void passwordValidator(String pass){
+        // use a variable to access java built in function length
+        //check if the pass has lower case and uppercase
+        // check if pass has special characters
+        //loop through each character of the pass and check what it is
+        //assign each of these values to a number
+        //if number is above a certain threshold (>=8 , ...)
+        //sum together to get a total or if higher print if 1 && 2 print....
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //call getPass
+        String pass = getPass();
+        //call passwordValidation to do all the heavy lifting
+    }
+}
