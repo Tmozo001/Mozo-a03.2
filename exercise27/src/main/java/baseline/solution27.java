5c1b78a406d680c0e2e9d3715c451646faf2c68f
Tyler Mozo <Tmozo001@gmail.com>
2021-10-01 18:28:15 -0400

    skeleton o f27 done

--- exercise27/src/main/java/baseline/solution27.java
+++ exercise27/src/main/java/baseline/solution27.java
@@ -1,4 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Tyler Mozo
+ */
+
 package baseline;
+import java.util.Scanner;
 
 public class solution27 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static String getFirstName(){
+        //get first name and scna into firstName
+    }
+    private static String getLastName(){
+        //get last name and scan into lastName
+
+    }
+    private static String getZIP(){
+//get zip and scan into ZIP
+    }
+    private static String getUID() {
+//get uid form user and scan into UID
+    }
+
+    private static void validateInputs(String firstName, String lastName, String ZIP, String UID){
+        //takes in the validation of each case and verifies.
+    }
+
+    private static boolean validateFirstName(firstName){
+        //makes sure firstname is longer than 2 characters
+    }
+    private static boolean validateLastName(lastName){
+//makes sure lastname is longer than 2 characters
+    }
+    private static boolean validateZIP(ZIP){
+//makes sure zip is 5 digits
+    }
+    private static boolean validateUID(UID){
+    //makes sure id is in correct format
+    }
+
+    public static void main(String[] args) {
+        // call validateInputs to do all the work
+    }
+
+
+
 }
