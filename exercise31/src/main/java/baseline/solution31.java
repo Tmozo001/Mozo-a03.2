4d8fb6ab36364d2a8932fe2b67af9c10407faa10
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 19:07:16 -0400

    ecercise31 done

--- exercise31/src/main/java/baseline/solution31.java
+++ exercise31/src/main/java/baseline/solution31.java
@@ -1,6 +1,6 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Tyler Mozo
  */
 package baseline;
 import java.util.*;
@@ -10,30 +10,46 @@ public class solution31 {
 
     private static int getRP(){
         //ask user for resting heart rate
+        System.out.print("Resting Pulse: ");
         //scan into variable RP
-        //check if rp is between 55 & 95
-        //if not print invalid
+        int RP = input.nextInt();
 
-    }
+        return RP;
+        }
 
     private static int getAge() {
-        //ask user for age untill a valid number is entered
+        //ask user for age until a valid number is entered
+        System.out.print("Age: ");
+        int age = input.nextInt();
         //if invalid, print invalid and ask again
+        if (age<1){
+            System.out.print("Invalid Input!");
+            getAge();
+        }
 
-        //return age;
+        return age;
     }
 
-    private static void calcTargetHeartRate(){
+    private static void calcTargetHeartRate(int RP, int age){
         //take in RP and age
         //loop through each iteration of intensity increasing 5% each time
+        for(int intensity = 50 ; intensity<=95 ; intensity+=5){
+            int TargetHeartRate = (int) ((((220 - age) - RP) * (intensity/100.0)) + RP);
+            System.out.printf("%d%%\t\t| %d bpm\n", intensity, TargetHeartRate);
+        }
         //print ("%d%%\t\t| %f bpm", intensity, TargetHeartRate)
 
     }
 
     public static void main(String[] args) {
         //call getRP
+        int RP = getRP();
         //call getAge
-        //build first part of table print("Intensity\t | Rate\n-------------|--------");
+        int age = getAge();
+        //build first part of table
+        System.out.print("Intensity\t | Rate\n-------------|--------");
+
         //call calcTargetHeartRate
+        calcTargetHeartRate(RP, age);
     }
 }
