71d0d3fd5c88ac3b8bf45a670a885c9e54312c00
Tyler Mozo <Tmozo001@gmail.com>
2021-10-01 21:49:28 -0400

    craeted exercise 28

---
+++ exercise28/src/main/java/baseline/solution28.java
@@ -0,0 +1,13 @@
+package baseline;
+import java.util.Scanner;
+
+public class solution28 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static
+    public static void main(String[] args) {
+        
+    }
+
+
+}
