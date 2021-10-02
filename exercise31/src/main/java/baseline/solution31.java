8457b759c3ab0af7c67e5e8772e821bcdb81fb90
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 00:37:58 -0400

    started 31

---
+++ exercise31/src/main/java/baseline/solution31.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+
+public class solution31 {
+    Scanner input = new Scanner(System.in);
+}
