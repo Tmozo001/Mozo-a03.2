db39ddfc8f0cbbe3c1344683b040f56d576e9c58
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 00:18:08 -0400

    Skeleton for 30 done

---
+++ exercise30/src/main/java/baseline/solution30.java
@@ -0,0 +1,19 @@
+package baseline;
+import java.util.*;
+
+
+public class solution30 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static void build(){
+        //run a loop increasing i from 1 to 12
+        //nested loop increases j to 12
+        //perform multiplication
+        //allign table
+    }
+
+    public static void main(String[] args) {
+        build();
+    }
+
+}
