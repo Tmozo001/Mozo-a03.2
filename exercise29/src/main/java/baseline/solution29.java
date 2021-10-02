7d2180708c067809700e9b32570bc6ab27eee00c
Tyler Mozo <Tmozo001@gmail.com>
2021-10-01 23:23:24 -0400

    skeleton of 29 complete

---
+++ exercise29/src/main/java/baseline/solution29.java
@@ -0,0 +1,31 @@
+package baseline;
+import java.util.Scanner;
+
+public class solution29 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static String getRate(){
+        //ask user for rate
+        //assign to variable rate
+        //return rate
+    }
+
+    private static boolean inputValidity(){
+        //check if rate is an invalid number
+        //check for division error
+        //call calcRate to get the rate
+    }
+
+    private static double calcRate(){
+        //take in the rate inputed after verification
+        //   72/rate
+        //print rate with appropriate output
+    }
+
+    public static void main(String[] args) {
+        //call getRate
+        //call input validity
+    }
+
+
+}
