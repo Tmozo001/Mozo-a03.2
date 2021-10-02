8550afe84c8672328a8906785bbd326ae0c9c524
Tyler Mozo <Tmozo001@gmail.com>
2021-10-01 22:21:28 -0400

    finished exercise 28

--- exercise28/src/main/java/baseline/solution28.java
+++ exercise28/src/main/java/baseline/solution28.java
@@ -5,14 +5,24 @@ public class solution28 {
     public static Scanner input = new Scanner(System.in);
 
     private static int loop() {
-        //for loop asking user to enter a number 5 times
-        //add each new number onto total
+        int total = 0;
+//for loop asking user to enter a number 5 times
+        for (int i = 0; i <=4 ; i++) {
+            System.out.print("Enter a number: ");
+            //add each new number onto total
+            total += input.nextInt();
+        }
         //return total
+        return total;
+
+
+
     }
     public static void main(String[] args) {
         // call loop
+        int total = loop();
         //print the total
-
+        System.out.printf("The total is %d", total);
     }
 
 
