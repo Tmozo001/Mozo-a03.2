29a456cf4f05660a443f30d11fec021628fd455e
Tyler Mozo <Tmozo001@gmail.com>
2021-10-01 22:19:47 -0400

    finished skeleton of 28

--- exercise28/src/main/java/baseline/solution28.java
+++ exercise28/src/main/java/baseline/solution28.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class solution28 {
     public static Scanner input = new Scanner(System.in);
 
-    private static
+    private static int loop() {
+        //for loop asking user to enter a number 5 times
+        //add each new number onto total
+        //return total
+    }
     public static void main(String[] args) {
-        
+        // call loop
+        //print the total
+
     }
 
 
