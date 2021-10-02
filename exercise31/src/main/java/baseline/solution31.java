dffba81bd88c802405c2feac607de2cdd75328bd
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 18:54:23 -0400

    skeleton of 31 done

--- exercise31/src/main/java/baseline/solution31.java
+++ exercise31/src/main/java/baseline/solution31.java
@@ -6,13 +6,34 @@ package baseline;
 import java.util.*;
 
 public class solution31 {
-    Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
 
     private static int getRP(){
+        //ask user for resting heart rate
+        //scan into variable RP
+        //check if rp is between 55 & 95
+        //if not print invalid
 
     }
-    private static int getAge(){
+
+    private static int getAge() {
+        //ask user for age untill a valid number is entered
+        //if invalid, print invalid and ask again
+
+        //return age;
+    }
+
+    private static void calcTargetHeartRate(){
+        //take in RP and age
+        //loop through each iteration of intensity increasing 5% each time
+        //print ("%d%%\t\t| %f bpm", intensity, TargetHeartRate)
 
     }
-    private 
+
+    public static void main(String[] args) {
+        //call getRP
+        //call getAge
+        //build first part of table print("Intensity\t | Rate\n-------------|--------");
+        //call calcTargetHeartRate
+    }
 }
