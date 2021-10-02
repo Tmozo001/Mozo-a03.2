9feeb98f30e7f562ceef7b5c799c5ffa9f0a86c4
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 00:21:53 -0400

    Exercise 30 complete

--- exercise30/src/main/java/baseline/solution30.java
+++ exercise30/src/main/java/baseline/solution30.java
@@ -5,15 +5,24 @@ import java.util.*;
 public class solution30 {
     public static Scanner input = new Scanner(System.in);
 
-    private static void build(){
+    private static void buildTable(){
         //run a loop increasing i from 1 to 12
-        //nested loop increases j to 12
+        for(int i=1 ; i<=12 ; i++){
+            //nested loop increases j to 12
+            for(int j=1 ; j<=12 ; j++){
+                int product = i * j;
+                System.out.printf("%d\t",product);
+            }
+            //creates the table effect by creating a new line after i reaches next iteration
+            System.out.print("\n");
+        }
+
         //perform multiplication
         //allign table
     }
 
     public static void main(String[] args) {
-        build();
+        buildTable();
     }
 
 }
