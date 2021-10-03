1507b27f8957f4a102027375492e89800f9d1120
Tyler Mozo <Tmozo001@gmail.com>
2021-10-03 01:57:34 -0400

    skeleton for exercise38 done

---
+++ exercise38/src/main/java/baseline/solution38.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Tyler Mozo
+ */
+package baseline;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class solution38 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static Integer[] filterEvenNumbers(String spacedOutList[]) {
+        //initialize an arrayList
+        //loop through spacedoutlist checking each number if even (modulo 2)
+        //if even add to a new array
+        //return this array
+    }
+    public static void main(String[] args) {
+        //ask the user for the input numbers spaced out.
+        //store this into array spacedOutList
+        //call filterEvenNumbers
+        //print the array with required output
+
+    }
+}
