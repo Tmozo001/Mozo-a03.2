75c6b22ce625b920f1a36439153caa1f66e6a207
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 20:55:01 -0400

    skeleton of 33 done

---
+++ exercise33/src/main/java/baseline/solution33.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Tyler Mozo
+ */
+package baseline;
+import java.util.*;
+
+public class solution33 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static void magicBall(){
+        //create string array of randomResponses
+
+        //create an int that takes a random number up to the length of the array
+        //doing so selects one of the spots of the array
+
+        //write a string that takes the random reply gotten
+
+        //print the reply
+    }
+
+    public static void main(String[] args) {
+        //ask user for their question
+        //call magic ball method
+    }
+
+}
