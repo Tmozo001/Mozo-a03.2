21da4b8d9680ed678010e00158a92072eccf1a4e
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 22:39:22 -0400

    finsihed exercise 35

--- exercise35/src/main/java/baseline/solution35.java
+++ exercise35/src/main/java/baseline/solution35.java
@@ -8,21 +8,36 @@ import java.util.*;
 public class solution35 {
     public static Scanner input = new Scanner(System.in);
 
-    private static void createArray(){
+    private static ArrayList createArray(){
         //create a new array nameLottery
-        //fill this array while user inputs a name, add it.
-        //when user enters " " stop
+        ArrayList<String> nameLottery = new ArrayList<String>();
+        String name = null;
 
+        do {
+            System.out.println("Enter a name: ");
+            name= input.nextLine();
+            //fill this array while user inputs a name, add it.
+            if(!name.isBlank() && !name.isEmpty())
+                nameLottery.add(name);
+        } //when user enters " " stop
+        while(!name.isBlank() && !name.isEmpty());
+
+        return nameLottery;
     }
 
     private static int randIndex(ArrayList nameLottery){
-        // create random index and pick from the length of the array
+        // create random index and pick from the size of the array
+        Random name = new Random();
+
+        int winner = name.nextInt(nameLottery.size());
         //return winner
+        return winner;
     }
 
     public static void main(String[] args) {
-        //call createarray
-        //call randindex and get the winner
+        ArrayList nameLottery = createArray();
+        int winner = randIndex(nameLottery);
         //print required output with winner
+        System.out.println("The winner is... "+nameLottery.get(winner));
     }
 }
