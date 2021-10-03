02c25e6ebf02e47b492448abfee93f599698e897
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 22:00:28 -0400

    completed exercise 34

--- exercise34/src/main/java/baseline/solution34.java
+++ exercise34/src/main/java/baseline/solution34.java
@@ -10,29 +10,56 @@ public class solution34 {
 
     private static ArrayList createArray(){
         //initialize a new arraylist
+        ArrayList<String> employees = new ArrayList();
         //add each employee name
+        employees.add("John Smith");
+        employees.add("Jackie Jackson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+
         //get the length of this new array
+        int length = employees.toArray().length;
         //print the length with the appropriate statement
+        System.out.printf("There are %d employees: ", length);
+        System.out.println("\n"+employees);
+
         //return arraylist
+        return employees;
+
     }
 
 
     private static String getEvilEmployee() {
         //ask the user for who they want to remove
+        System.out.print("Enter an employee name to remove: ");
         //assign that to a string
+        String banished = input.nextLine();
+
         //return the string
+        return banished;
     }
 
 
     private static void banishEmployee(ArrayList employees, String banished){
         //use remove function to remove banished from the array
+        employees.remove(banished);
         //find the new length and print it
+        int length = employees.toArray().length;
         //print the new array
+        System.out.println(employees);
+
 
     }
 
     public static void main(String[] args) {
         //call the functions with appropraite parameters
+        ArrayList employees = createArray();
+
+        String banished = getEvilEmployee();
+
+        banishEmployee(employees, banished);
+
 
     }
 
