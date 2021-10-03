b6fc58465dc0a3e9a898f9f5516ddb7c768a273e
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 21:12:14 -0400

    Exercise 33 completed

--- exercise33/src/main/java/baseline/solution33.java
+++ exercise33/src/main/java/baseline/solution33.java
@@ -10,18 +10,29 @@ public class solution33 {
 
     private static void magicBall(){
         //create string array of randomResponses
+        String[] randomResponse = {"Yes.", "No.", "Maybe.", "Ask again later.", "I don't know, figure it out yourself.", "Bye!"};
+        Random response = new Random();
 
         //create an int that takes a random number up to the length of the array
         //doing so selects one of the spots of the array
+        int randomIndex = response.nextInt(randomResponse.length);
 
         //write a string that takes the random reply gotten
+        String reply = randomResponse[randomIndex];
 
         //print the reply
+        System.out.printf("%s",reply);
     }
 
     public static void main(String[] args) {
         //ask user for their question
+        System.out.print("What's your question?\n");
+        String doesNotMatter = input.nextLine();
+        
         //call magic ball method
+        magicBall();
+
+
     }
 
 }
