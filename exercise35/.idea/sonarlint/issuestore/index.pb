add61fc477b289211d816e01516bd1091be006ac
Tyler Mozo <Tmozo001@gmail.com>
2021-10-03 00:48:38 -0400

    skeleton of 37 done

---
+++ exercise35/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/solution35.java,9\7\971f6d0dd79a29011775fefb888c769d414092d0
---
+++ exercise37/src/main/java/baseline/solution37.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Tyler Mozo
+ */
+package baseline;
+import java.util.*;
+
+public class solution37 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static int getPassLength(){
+        //ask the user for desired length of password
+        //return length
+    }
+    private static int getSpecialCharacterCount(){
+        //ask the user for desired number of special characters for password
+        //return special
+    }
+    private static int getNumberCount(){
+        //ask the user for desired number of numbers for password
+        //return numbers
+    }
+
+    private static String passwordFactory(int length, int special, int numbers)
+    {
+        //create a string of all the desired characters to be randomized
+        //create a string of all the desired special characters to be randomized
+        //create a string of all the desired numbers to be randomized
+
+        //create a random object
+        //add randomly selected elements to an array
+        //print new pass
+    }
+}
