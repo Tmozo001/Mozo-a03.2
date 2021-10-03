475423731af64a73e7e059a43cc58dc2aa3bfaf2
Tyler Mozo <Tmozo001@gmail.com>
2021-10-03 02:03:54 -0400

    finished exercise38

--- exercise38/src/main/java/baseline/solution38.java
+++ exercise38/src/main/java/baseline/solution38.java
@@ -11,15 +11,52 @@ public class solution38 {
 
     private static Integer[] filterEvenNumbers(String spacedOutList[]) {
         //initialize an arrayList
+        ArrayList<Integer> newList = new ArrayList<>();
+
         //loop through spacedoutlist checking each number if even (modulo 2)
-        //if even add to a new array
+        for(int i=0; i<spacedOutList.length; i++){
+
+            int place = Integer.parseInt(spacedOutList[i]);
+            //if even add to a new array
+            if(place % 2 == 0){
+
+                newList.add(place);
+            }
+        }
+
+        Integer[] evenNumbers = new Integer[newList.size()];
+        evenNumbers = newList.toArray(evenNumbers);
         //return this array
+        return evenNumbers;
+
+
+
+
     }
     public static void main(String[] args) {
         //ask the user for the input numbers spaced out.
+        System.out.print("Enter a list of numbers, separated by spaces: ");
         //store this into array spacedOutList
+        String numbers = input.nextLine();
+
+        String spacedOutList[] = numbers.split(" ");
         //call filterEvenNumbers
-        //print the array with required output
+        Integer evenNumbers[] = filterEvenNumbers(spacedOutList);
+
+        System.out.println("The even numbers are ");
+        for(int i=0; i<evenNumbers.length; i++){
+            if(i < evenNumbers.length-1){
+                System.out.print(evenNumbers[i] + " ");
+            }
+            else
+            {
+                System.out.print(evenNumbers[i] + ".");
+            }
+        }
+
+
+
+
 
     }
 }
