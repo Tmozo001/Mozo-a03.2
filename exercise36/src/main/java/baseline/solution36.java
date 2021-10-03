ec6e122d1d1ebcc71fe08c11363b68af818d2778
Tyler Mozo <Tmozo001@gmail.com>
2021-10-02 23:34:57 -0400

    skeleton for 36

---
+++ exercise36/src/main/java/baseline/solution36.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Tyler Mozo
+ */
+package baseline;
+import java.util.*;
+
+public class solution36 {
+    public static Scanner input = new Scanner(System.in);
+
+    private static ArrayList createArray(){
+        //initialize array responseTimes
+
+        //do while to fill the array with the response times
+        //if the response time is not done add it to the array
+        //do while the responsetime != done
+        //print the array with Numbers: ...
+        return responseTimes;
+    }
+    private static double average(ArrayList responseTimes){
+        //loop through the array adding each piece together and then divide by array size
+
+    }
+    private static double minimum(ArrayList responseTimes){
+       //loop through the array comparing which is the smallest
+
+    }
+    private static double maximum(ArrayList responseTimes){
+        //loop through the array comparing which is the largest
+
+    }
+    private static double standardDeviation(ArrayList responseTimes){
+       //use standard deviation formula for the values in the loop
+
+    }
+
+    public static void main(String[] args) {
+        //call create array
+        //call each math method
+    }
+
+}
